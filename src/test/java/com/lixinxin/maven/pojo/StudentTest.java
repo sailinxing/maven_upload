78424354f9a6c3c99727d347cd97aa8414b76c2a
Administrator <Administrator@PC>
2018-01-05 20:52:56 +0800

    maven项目

---
+++ src/test/java/com/lixinxin/maven/pojo/StudentTest.java
@@ -0,0 +1,12 @@
+package com.lixinxin.maven.pojo;
+
+import org.junit.Test;
+
+public class StudentTest {
+/*@Test
+public void test1(){
+	Student student=new Student();
+	student.setName("sjfsj");
+	System.out.println(student);
+}*/
+}
